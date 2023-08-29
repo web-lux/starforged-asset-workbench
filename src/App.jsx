b486f9ee29f9f0044a4a8fc8739a35e79945f86d
Lux <web-lux@outlook.fr>
2023-08-29 21:48:08 +0200

    feat: preview with placeholder values

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Preview from './components/Preview';
+
+function Form() {
+  return (
+    <div>Form</div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="wrapper">
+      <header>
+        <h1>Ironsworn : Starforged Asset Workbench</h1>
+      </header>
+
+      <main>
+        <Preview />
+        <Form />
+      </main>
+
+      <footer>Footer</footer>
+    </div>
   )
 }
 
---
+++ src/components/Preview.jsx
@@ -0,0 +1,54 @@
+import "./Preview.scss";
+import icon from "../assets/owl.svg";
+
+export default function Preview() {
+    return (
+        <section className="asset-preview">
+
+            <header>
+
+                <div className="title-container">
+                    <span className="asset-type">Module</span>
+                    <span className="asset-title">Engine Upgrade</span>
+                </div>
+
+                <div className="clip icon-container">
+                    <div className="clip icon">
+                        <img src={icon} />
+                        <svg xmlns="http://www.w3.org/2000/svg" height="0" width="0">
+                            <clipPath id="svg-hex" clipPathUnits="objectBoundingBox">
+                                <path
+                                    d="M0.501,0 L1,0.25 V0.75 L0.501,1 L0.001,0.75 L0.001,0.25 L0.501,0"
+                                />
+                            </clipPath>
+                        </svg>
+                    </div>
+                </div>
+
+            </header>
+
+            <ul className="description">
+            <li className="checked">
+					Your vehicle’s finely-tuned engines speed your travels. When you
+					Undertake an Expedition (+edge) and score a strong hit, take +1
+					momentum; on a strong hit with a 6 on your action die, take +2
+					momentum instead of +1.
+				</li>
+				<li>
+					When you Enter the Fray, choose one (before rolling).
+					<ul>
+						<li>Maneuver: Add +1 and take +1 momentum on a strong hit.</li>
+						<li>Boost: Take +2 momentum on a hit.</li>
+					</ul>
+				</li>
+				<li>
+					When you make a desperate move to pursue a foe, escape a threat, or
+					get in range, you may push your engines to their limit. If you do
+					(decide after rolling), reroll any dice and count a weak hit as a
+					strong hit. Then, Withstand Damage (-2).
+				</li>
+            </ul>
+
+        </section>
+    )
+}
