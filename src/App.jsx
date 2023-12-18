43b76167810a5d537a8e908e50fa96d73d64d8dc
lux <web-lux@outlook.fr>
2023-12-18 17:43:12 +0100

    chore: update dead link

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
 
 			<footer>
 				<span>
-					Created by <a href="https://web-lux.fr/">web-lux</a>. With help from <a href="https://github.com/elodie-28">elodie-28</a>. For{" "}
+					Created by <a href="https://github.com/web-lux">web-lux</a>. With help from <a href="https://github.com/elodie-28">elodie-28</a>. For{" "}
 					<a href="https://www.ironswornrpg.com/">Ironsworn: Starforged</a> by
 					Shawn Tomkin.
 				</span>
