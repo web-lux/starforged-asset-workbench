5aa24789992c4ab132773fa15dfa0a31a003b50f
web-lux <web-lux@outlook.fr>
2024-02-22 23:03:28 +0100

    ref: type placeholder asset

---
+++ src/interfaces/Asset.tsx
@@ -0,0 +1,21 @@
+type Field = {
+	id: number;
+	isChecked: boolean;
+	text: string;
+};
+
+export default interface Asset {
+	title: string;
+	type: string;
+	description: string;
+	image: {
+		path: string;
+		size: number;
+	};
+	track: {
+		isChecked: boolean;
+		maxNumber: number;
+	};
+	additionalFields: Field[];
+	upgrades: Field[];
+}
