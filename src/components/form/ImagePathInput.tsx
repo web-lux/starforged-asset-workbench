cf2c90facacaf3ff816514813e28b2459a073ad5
web-lux <web-lux@outlook.fr>
2024-02-25 16:24:35 +0100

    feat: change image url for image upload

--- src/components/form/ImagePathInput.tsx
+++ src/components/form/ImagePathInput.tsx
@@ -3,15 +3,25 @@ import hasUpdateAsset from "../../interfaces/HasUpdateAsset";
 export default function ImagePathInput({ updateAsset }: hasUpdateAsset) {
 	return (
 		<div>
-			<label htmlFor="assetImage">Image url</label>
+			<label htmlFor="assetImage">Image</label>
 			<input
-				type="text"
+				type="file"
 				name="assetImage"
 				id="assetImage"
+				accept="image/png, image/jpeg, image/svg+xml"
 				onChange={(e) => {
-					updateAsset((draft) => {
-						draft.image.path = e.target.value;
-					});
+					const file = e.target.files[0];
+
+					if (file) {
+						const reader = new FileReader();
+						reader.readAsDataURL(file);
+
+						reader.onload = (e) => {
+							updateAsset((draft) => {
+								draft.image.path = e.target.result.toString();
+							});
+						};
+					}
 				}}
 			/>
 		</div>
