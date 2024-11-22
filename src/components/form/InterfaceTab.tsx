24516e44d1c9894277ac97592250f2c6b2571663
Lux <web-lux@outlook.fr>
2024-11-22 11:17:32 +0100

    feat: custom name for asset type

--- src/components/form/InterfaceTab.tsx
+++ src/components/form/InterfaceTab.tsx
@@ -1,159 +1,183 @@
-import hasUpdateAsset from "../../interfaces/HasUpdateAsset";
-import ImagePathInput from "./ImagePathInput";
+import { useState } from 'react';
+import hasUpdateAsset from '../../interfaces/HasUpdateAsset';
+import ImagePathInput from './ImagePathInput';
 
 interface Props extends hasUpdateAsset {
-	tab: string;
+    tab: string;
 }
 
 export default function InterfaceTab({ updateAsset, tab }: Props) {
-	const possibleTypes = [
-		"Command Vehicle",
-		"Module",
-		"Support Vehicle",
-		"Path",
-		"Companion",
-		"Deed",
-	];
+    const defaultTypes = ['Command Vehicle', 'Module', 'Support Vehicle', 'Path', 'Companion', 'Deed'];
+    const [isTypeCustom, setIsTypeCustom] = useState(false);
 
-	const typeOptions = possibleTypes.map((type, index) => (
-		<option
-			value={type}
-			key={index}>
-			{type}
-		</option>
-	));
+    const typeOptions = [...defaultTypes, 'Custom'].map((type, index) => (
+        <option
+            value={type}
+            key={index}>
+            {type}
+        </option>
+    ));
 
-	function getAdditionalFieldInputs() {
-		let inputs = [];
+    function handleTypeChange(e) {
+        if (defaultTypes.includes(e.target.value)) {
+            setIsTypeCustom(false);
+        } else setIsTypeCustom(true);
+        updateAsset((draft) => {
+            draft.type = e.target.value;
+        });
+    }
 
-		for (let i = 0; i < 2; i++) {
-			inputs.push(
-				<div key={i}>
-					<label htmlFor="assetAdditionalField1">
-						Additional field {i + 1}
-					</label>
-					<input
-						type="checkbox"
-						name={`AdditionalFieldCheck${i}`}
-						id={`AdditionalFieldCheck${i}`}
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.additionalFields[i].isChecked = e.target.checked;
-							})
-						}
-					/>
-					<input
-						type="text"
-						name={`AdditionalFieldField${i}`}
-						id={`AdditionalFieldField${i}`}
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.additionalFields[i].text = e.target.value;
-							})
-						}
-					/>
-				</div>
-			);
-		}
+    function getCustomTypeNameInput() {
+        if (isTypeCustom) {
+            return (
+                <div>
+                    <label htmlFor="customTypeName">Custom Name</label>
+                    <input
+                        type="text"
+                        name="customTypeName"
+                        id="customTypeName"
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.type = e.target.value;
+                            })
+                        }
+                    />
+                </div>
+            );
+        }
+        return null;
+    }
 
-		return inputs;
-	}
+    function getAdditionalFieldInputs() {
+        let inputs = [];
 
-	return (
-		<fieldset className={tab === "interface" ? "visible" : null}>
-			<legend hidden>Interface</legend>
+        for (let i = 0; i < 2; i++) {
+            inputs.push(
+                <div key={i}>
+                    <label htmlFor="assetAdditionalField1">Additional field {i + 1}</label>
+                    <input
+                        type="checkbox"
+                        name={`AdditionalFieldCheck${i}`}
+                        id={`AdditionalFieldCheck${i}`}
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.additionalFields[i].isChecked = e.target.checked;
+                            })
+                        }
+                    />
+                    <input
+                        type="text"
+                        name={`AdditionalFieldField${i}`}
+                        id={`AdditionalFieldField${i}`}
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.additionalFields[i].text = e.target.value;
+                            })
+                        }
+                    />
+                </div>
+            );
+        }
 
-			<div className="fieldgroup">
-				<ImagePathInput updateAsset={updateAsset} />
+        return inputs;
+    }
 
-				<div>
-					<label htmlFor="assetImageSize">Image Size</label>
-					<input
-						type="range"
-						name="assetImageSize"
-						id="assetImageSize"
-						onChange={(e) => {
-							updateAsset((draft) => {
-								draft.image.size = Number(e.target.value);
-							});
-						}}
-					/>
-				</div>
-			</div>
+    return (
+        <fieldset className={tab === 'interface' ? 'visible' : null}>
+            <legend hidden>Interface</legend>
 
-			<div className="fieldgroup">
-				<div>
-					<label htmlFor="assetType">Type (module, path...)</label>
-					<select
-						name="assetType"
-						id="assetType"
-						defaultValue={possibleTypes[1]}
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.type = e.target.value;
-							})
-						}>
-						{typeOptions}
-					</select>
-				</div>
+            <div className="fieldgroup">
+                <ImagePathInput updateAsset={updateAsset} />
 
-				<div>
-					<label htmlFor="assetTitle">Title</label>
-					<input
-						type="text"
-						name="assetTitle"
-						id="assetTitle"
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.title = e.target.value;
-							})
-						}
-					/>
-				</div>
-			</div>
+                <div>
+                    <label htmlFor="assetImageSize">Image Size</label>
+                    <input
+                        type="range"
+                        name="assetImageSize"
+                        id="assetImageSize"
+                        onChange={(e) => {
+                            updateAsset((draft) => {
+                                draft.image.size = Number(e.target.value);
+                            });
+                        }}
+                    />
+                </div>
+            </div>
 
-			<div className="fieldgroup">
-				<div>
-					<label htmlFor="assetDescription">Description</label>
-					<input
-						type="text"
-						name="assetDescription"
-						id="assetDescription"
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.description = e.target.value;
-							})
-						}
-					/>
-				</div>
-				<div>
-					<label htmlFor="assetTrack">Track</label>
-					<input
-						type="checkbox"
-						name="AssetTrackCheck"
-						id="AssetTrackCheck"
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.track.isChecked = e.target.checked;
-							})
-						}
-					/>
-					<input
-						type="number"
-						name="assetTrack"
-						id="assetTrack"
-						min="1"
-						max="6"
-						onChange={(e) =>
-							updateAsset((draft) => {
-								draft.track.maxNumber = Number(e.target.value);
-							})
-						}
-					/>
-				</div>
-			</div>
+            <div className="fieldgroup">
+                <div>
+                    <label htmlFor="assetType">Type (module, path...)</label>
+                    <select
+                        name="assetType"
+                        id="assetType"
+                        defaultValue={defaultTypes[1]}
+                        onChange={(e) => handleTypeChange(e)}>
+                        {typeOptions}
+                    </select>
+                </div>
 
-			<div className="fieldgroup">{getAdditionalFieldInputs()}</div>
-		</fieldset>
-	);
+                {getCustomTypeNameInput()}
+            </div>
+
+            <div className="fieldgroup">
+                <div>
+                    <label htmlFor="assetTitle">Title</label>
+                    <input
+                        type="text"
+                        name="assetTitle"
+                        id="assetTitle"
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.title = e.target.value;
+                            })
+                        }
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="assetDescription">Description</label>
+                    <input
+                        type="text"
+                        name="assetDescription"
+                        id="assetDescription"
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.description = e.target.value;
+                            })
+                        }
+                    />
+                </div>
+            </div>
+
+            <div className="fieldgroup">
+                <div>
+                    <label htmlFor="assetTrack">Track</label>
+                    <input
+                        type="checkbox"
+                        name="AssetTrackCheck"
+                        id="AssetTrackCheck"
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.track.isChecked = e.target.checked;
+                            })
+                        }
+                    />
+                    <input
+                        type="number"
+                        name="assetTrack"
+                        id="assetTrack"
+                        min="1"
+                        max="6"
+                        onChange={(e) =>
+                            updateAsset((draft) => {
+                                draft.track.maxNumber = Number(e.target.value);
+                            })
+                        }
+                    />
+                </div>
+            </div>
+
+            <div className="fieldgroup">{getAdditionalFieldInputs()}</div>
+        </fieldset>
+    );
 }
